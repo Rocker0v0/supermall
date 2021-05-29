1d4c1226781eeccaaf99b815ec5905a8bb09ad1f
Rocker0v0 <73229896+Rocker0v0@users.noreply.github.com>
2021-05-29 18:09:35 +0800

    更改URL

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config) {
   // 1.创建实例
   const instance = axios.create({
-    baseURL: "http://152.136.185.210:8000/api/w6",
+    baseURL: "***",
   })
 
   // 2.axios拦截器
